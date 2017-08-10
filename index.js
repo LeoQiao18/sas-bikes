521c549ffb6503a2a4c8b657b84864d7860903a1
Leo Qiao <qiaofeitong@hotmail.com>
2017-08-10 13:03:31 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('HI BIKE APP!');
+});
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
